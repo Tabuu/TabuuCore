5897b4acdf6bfd3ec1fe4fb65c2f35b8787c12d2
Rick van Sloten <rickvansloten@gmail.com>
2020-04-09 14:32:13 +0200

    Fixed byte serializers.

--- core/src/main/java/nl/tabuu/tabuucore/serialization/bytes/AbstractByteSerializer.java
+++ core/src/main/java/nl/tabuu/tabuucore/serialization/bytes/AbstractByteSerializer.java
@@ -1,13 +1,9 @@
 package nl.tabuu.tabuucore.serialization.bytes;
 
+import nl.tabuu.tabuucore.serialization.IObjectDeserializer;
 import nl.tabuu.tabuucore.serialization.IObjectSerializer;
-import nl.tabuu.tabuucore.serialization.ISerializer;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-
-public abstract class AbstractByteSerializer<T> implements IObjectSerializer<T, byte[]> {
+public abstract class AbstractByteSerializer<T> implements IObjectSerializer<T, byte[]>, IObjectDeserializer<byte[], T> {
     @Override
     public abstract byte[] serialize(T object);
 
