9355be296078423f41675b54b1295a3831f23bf4
Rick van Sloten <rickvansloten@gmail.com>
2023-06-13 23:30:26 +0200

    Added support for Minecraft 1.19.4

---
+++ v1_19_R3/src/main/java/nl/tabuu/tabuucore/nms/v1_19_R3/container/AnvilContainerWindow.java
@@ -0,0 +1,46 @@
+package nl.tabuu.tabuucore.nms.v1_19_R3.container;
+
+import net.minecraft.core.BlockPosition;
+import net.minecraft.network.chat.IChatBaseComponent;
+import net.minecraft.network.protocol.game.PacketPlayOutOpenWindow;
+import net.minecraft.world.inventory.ContainerAccess;
+import net.minecraft.world.inventory.ContainerAnvil;
+import net.minecraft.world.inventory.Containers;
+import net.minecraft.world.level.World;
+import nl.tabuu.tabuucore.nms.wrapper.container.IAnvilContainerWindow;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.AnvilInventory;
+
+public class AnvilContainerWindow extends ContainerWindow implements IAnvilContainerWindow {
+
+    private final ContainerAnvil _container;
+
+    protected AnvilContainerWindow(Player player) {
+        super(player);
+
+        World world = getEntityPlayer().H;
+        BlockPosition blockPosition = new BlockPosition(0, 0, 0);
+        _container = new ContainerAnvil(getId(), getEntityPlayer().fJ(), ContainerAccess.a(world, blockPosition));
+        _container.checkReachable = false;
+    }
+
+    @Override
+    protected ContainerAnvil getContainer() {
+        return _container;
+    }
+
+    @Override
+    protected void sendContainerWindowOpenPacket() {
+        getEntityPlayer().b.a(new PacketPlayOutOpenWindow(getId(), Containers.h, IChatBaseComponent.b(InventoryType.ANVIL.getDefaultTitle())));
+    }
+
+    @Override
+    public String getRenameText() {
+        if(!(getInventory() instanceof AnvilInventory))
+            return "";
+
+        AnvilInventory anvilInventory = (AnvilInventory) getInventory();
+        return anvilInventory.getRenameText();
+    }
+}
